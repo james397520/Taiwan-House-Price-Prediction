5fa1bc635af40d4d08d4a4dad35577b9828b03e7
james397520 <james397520@gmail.com>
2023-10-06 00:27:46 +0800

    feat: Update train.py

--- train.py
+++ train.py
@@ -3,23 +3,9 @@ import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from sklearn.preprocessing import StandardScaler
 from model import HousePriceModel
+from data import HousePriceDataset
 
-# Custom dataset
-class HousePriceDataset(Dataset):
-    def __init__(self, csv_file):
-        data = pd.read_csv(csv_file)
-        self.X = data.iloc[:, :-1].values
-        self.y = data.iloc[:, -1].values
 
-        # Standardize features
-        scaler = StandardScaler()
-        self.X = scaler.fit_transform(self.X)
-
-    def __len__(self):
-        return len(self.y)
-
-    def __getitem__(self, idx):
-        return torch.tensor(self.X[idx], dtype=torch.float32), torch.tensor(self.y[idx], dtype=torch.float32)
 
 # Hyperparameters
 batch_size = 64
