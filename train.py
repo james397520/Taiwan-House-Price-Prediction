90414b5fc2ba2d09363df5f9ec8605513049c5d6
james.chan <chan.jamesyc@inventec.com>
2023-10-12 19:27:56 +0800

    style: change file name from data.py to dataloader.py

--- train.py
+++ train.py
@@ -3,56 +3,73 @@ import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 from sklearn.preprocessing import StandardScaler
 from model import HousePriceModel
-from data import HousePriceDataset
+from dataloader import HousePriceTrainDataset
+import platform
+
+def main():
+
+    # Hyperparameters
+    batch_size = 128
+    learning_rate = 0.001
+    epochs = 100
+
+    data = pd.read_csv('data/training_data.csv')
+    # 指定要標準化的列和標準化方法
+    normalize_columns = {
+    '橫坐標': 'min-max', #z-score
+    '縱坐標': 'min-max'
+    }
+
+    # 選擇要用作特徵的列
+    selected_features = ['橫坐標', '縱坐標']
+    target_column = '單價'
+    if platform.system() == "Linux":
+        gpu = True
+    elif platform.system() == "Darwin":
+        gpu = False
+
+    # 創建標準化後的數據集
+    train_dataset = HousePriceTrainDataset(data, selected_features, target_column, normalize_columns)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+    # print(train_dataset[0]["features"].shape)
+    # Initialize model
+    if gpu:
+        model = HousePriceModel(train_dataset[0]["features"].shape[0]).cuda()
+    else:
+        model = HousePriceModel(train_dataset[0]["features"].shape[0])
+
+    # Loss and optimizer
+    criterion = torch.nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Training loop
+    for epoch in range(epochs):
+        for batch in train_loader:
+            if gpu:
+                data = batch['features'].cuda()
+                targets = batch['target'].cuda()
+            else:
+                data = batch['features']
+                targets = batch['target']
+        # for batch_idx, (data, targets) in enumerate(train_loader):
+        #     data, targets = data.cuda(), targets.cuda() #cuda()
+        #     print(data)
+            # Forward pass
+            scores = model(data)
+            loss = criterion(scores.squeeze(1), targets)
+
+            # Backward pass
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+
+    # Save the trained model
+    torch.save(model.state_dict(), 'model.pth')
 
 
 
-# Hyperparameters
-batch_size = 64
-learning_rate = 0.001
-epochs = 100
-
-data = pd.read_csv('data/training_data.csv')
-# 指定要標準化的列和標準化方法
-normalize_columns = {
-'橫坐標': 'min-max', #z-score
-'縱坐標': 'min-max'
-}
-
-# 選擇要用作特徵的列
-selected_features = ['橫坐標', '縱坐標']
-target_column = '單價'
-
-# 創建標準化後的數據集
-train_dataset = HousePriceDataset(data, selected_features, target_column, normalize_columns, train=True)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
-# print(train_dataset[0]["features"].shape)
-# Initialize model
-model = HousePriceModel(train_dataset[0]["features"].shape[0])#.cuda()
-
-# Loss and optimizer
-criterion = torch.nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-# Training loop
-for epoch in range(epochs):
-    for batch in train_loader:
-        data = batch['features']
-        targets = batch['target']
-    # for batch_idx, (data, targets) in enumerate(train_loader):
-    #     data, targets = data, targets #cuda()
-    #     print(data)
-        # Forward pass
-        scores = model(data)
-        loss = criterion(scores.squeeze(1), targets)
-
-        # Backward pass
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
-
-# Save the trained model
-torch.save(model.state_dict(), 'model.pth')
+if __name__ == "__main__":
+    main()
